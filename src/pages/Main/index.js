69e51cc9cb034a85b58e01dcac8c33c7b8ffd02a
André <andrekunitz@gmail.com>
2019-04-11 21:09:46 -0300

    Criando tela principal

---
+++ src/pages/Main/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+export default class Main extends Component {
+  render() {
+    return (
+        <div id="main-container">
+            <form action="">
+              <img src={logo}></img>
+              <input 
+                placeholder="Criar uma box"
+              />
+              <button type="submit">Criar</button>
+            </form>
+        </div>
+    );
+  }
+}
